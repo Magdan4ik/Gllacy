f89a407048e385f26d4b491cafb33c332202067e
Vladislav Magdenko <vladd0193@gmail.com>
2017-11-29 00:01:55 +0200

    Переключение background и слайдов

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#bg1").click(function(){
+        $("body").addClass("bg-1");
+        $("body").removeClass("bg-2");
+        $("body").removeClass("bg-3"); 
+        $("#h-1").show();
+        $("#h-2").hide();
+        $("#h-3").hide();
+    });
+    $("#bg2").click(function(){
+        $("body").addClass("bg-2");
+        $("body").removeClass("bg-1");
+        $("body").removeClass("bg-3");
+        $("#h-2").show();
+        $("#h-1").hide();
+        $("#h-3").hide();   
+        $(".main-slider__indicator").removeClass("active-slide");
+    });
+    $("#bg3").click(function(){
+        $("body").addClass("bg-3");
+        $("body").removeClass("bg-1");
+        $("body").removeClass("bg-2");
+        $("#h-3").show();
+        $("#h-1").hide();
+        $("#h-2").hide();
+        $(".main-slider__indicator").removeClass("active-slide");
+    });
+});
