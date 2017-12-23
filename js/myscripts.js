8a98b2ad32f57c5099f3a261e1145921cf531042
Vladislav Magdenko <vladd0193@gmail.com>
2017-12-23 21:05:53 +0200

    Slider changes

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,32 +1,48 @@
 // SLIDER
 
 $(document).ready(function(){
+
     $("#bg1").click(function(){
         $("body").addClass("bg-1");
-        $("body").removeClass("bg-2");
-        $("body").removeClass("bg-3"); 
+        $("body").removeClass("bg-2 bg-3");
         $("#h-1").show();
         $("#h-2").hide();
         $("#h-3").hide();
+        $("#bg2").removeClass("active-slide");    
+        $("#bg3").removeClass("active-slide");
+        $("#bg1").addClass("active-slide");    
     });
+
     $("#bg2").click(function(){
         $("body").addClass("bg-2");
-        $("body").removeClass("bg-1");
-        $("body").removeClass("bg-3");
+        $("body").removeClass("bg-1 bg-3");
         $("#h-2").show();
         $("#h-1").hide();
-        $("#h-3").hide();   
-        $(".main-slider__indicator").removeClass("active-slide");
+        $("#h-3").hide();
+        $("#bg1").removeClass("active-slide");    
+        $("#bg3").removeClass("active-slide");
+        $("#bg2").addClass("active-slide");    
     });
+
     $("#bg3").click(function(){
         $("body").addClass("bg-3");
-        $("body").removeClass("bg-1");
-        $("body").removeClass("bg-2");
+        $("body").removeClass("bg-1 bg-2");
         $("#h-3").show();
         $("#h-1").hide();
         $("#h-2").hide();
-        $(".main-slider__indicator").removeClass("active-slide");
+        $("#bg1").removeClass("active-slide"); 
+        $("#bg2").removeClass("active-slide"); 
+        $("#bg3").addClass("active-slide");  
     });
+
+
+//Preload images
+  pic = new Image();
+  pic2 = new Image();
+  pic3 = new Image();
+    pic.src="/img/ice-cream-1.png";
+    pic2.src="/img/ice-cream-2.png";
+    pic3.src="/img/ice-cream-3.png";
 });
 
 
